125c735bccb19265c497233ec17915dd434bad42
Yugenn <yuruseajyan@gmail.com>
2021-09-25 17:57:31 +0900

    CRUD完成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\HtmlString;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ArticleController extends Controller
 {
@@ -13,7 +16,59 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $method = 'GET';
+        $tag_id = 'PHP';
+        $per_page = 30;
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/tags/' . $tag_id . '/items?per_page=' . $per_page;
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $articles = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $articles = null;
+        }
+        ###ここから取得
+
+        $method = 'GET';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/authenticated_user/items';
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $my_articles = json_decode($body, false);
+        } catch (\Throwable $th) {
+            $my_articles = null;
+        }
+        return view('articles.index')->with(compact('articles', 'my_articles'));
     }
 
     /**
@@ -23,7 +78,7 @@ class ArticleController extends Controller
      */
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     /**
@@ -34,7 +89,51 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $method = 'POST';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/items';
+
+        // スペース区切りの文字列を配列に変換し、JSON形式に変換
+        $tag_array = explode(' ', $request->tags);
+        $tags = array_map(function ($tag) {
+            return ['name' => $tag];
+        }, $tag_array);
+
+        // 送信するデータを整形
+        $data = [
+            'title' => $request->title,
+            'body' => $request->body,
+            'private' => $request->private == "true" ? true : false,
+            'tags' => $tags
+        ];
+
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+            'json' => $data,
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        try {
+            // データを送信する
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+
+            // GuzzleHttpで発生したエラーの場合はcatchする
+            // return back()->withErrors(['error' => $e->getResponse()->getReasonPhrase()]);
+            return back()->withErrors(['error' => "投稿に失敗しました"]);
+        }
+        $message = new \Illuminate\Support\HtmlString(
+            '記事の投稿に成功しました'
+        );
+        return redirect()->route('articles.index')->with('flash_message', '<a href="https://qiita.com/private/記事の投稿に成功しました。');
     }
 
     /**
@@ -45,7 +144,68 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        $method = 'GET';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+
+            $parser = new \cebe\markdown\GithubMarkdown();
+            $parser->keepListStartNumber = true;
+            $parser->enableNewlines = true;
+
+            $html_string = $parser->parse($article->body);
+            $article->html = new \Illuminate\Support\HtmlString($html_string);
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        ###ユーザー情報を取得
+
+
+        $method = 'GET';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/authenticated_user';
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $user = json_decode($body, false);
+        } catch (\Throwable $th) {
+            // dd($th);
+            return back();
+        }
+
+        return view('articles.show')->with(compact('article', 'user'));
     }
 
     /**
@@ -56,7 +216,37 @@ class ArticleController extends Controller
      */
     public function edit($id)
     {
-        //
+        $method = 'GET';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        // $optionsにトークンを指定
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        // try catchでエラー時の処理を書く
+        try {
+            // データを取得し、JSON形式からPHPの変数に変換
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+
+            $tag_array = array_map(function ($tag) {
+                return $tag->name;
+            }, $article->tags);
+            $article->tags = implode(' ', $tag_array);
+        } catch (\Throwable $th) {
+            return back();
+        }
+
+        return view('articles.edit')->with(compact('article'));
     }
 
     /**
@@ -68,7 +258,49 @@ class ArticleController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $method = 'PATCH';
+
+        // QIITA_URLの値を取得してURLを定義
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        // スペース区切りの文字列を配列に変換し、JSON形式に変換
+        $tag_array = explode(' ', $request->tags);
+        $tags = array_map(function ($tag) {
+            return ['name' => $tag];
+        }, $tag_array);
+
+        // 送信するデータを整形
+        $data = [
+            'title' => $request->title,
+            'body' => $request->body,
+            'private' => $request->private == "true" ? true : false,
+            'tags' => $tags
+        ];
+
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer ' . config('qiita.token'),
+            ],
+            'json' => $data,
+        ];
+
+        // Client(接続する為のクラス)を生成
+        $client = new Client();
+
+        try {
+            // データを送信する
+            $response = $client->request($method, $url, $options);
+            $body = $response->getBody();
+            $article = json_decode($body, false);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+
+            // GuzzleHttpで発生したエラーの場合はcatchする
+            // return back()->withErrors(['error' => $e->getResponse()->getReasonPhrase()]);
+            return back()->withErrors(['error' => "投稿に失敗しました"]);
+        }
+        $message = '記事の投稿に成功しました';
+        return redirect()->route('articles.index')->with('flash_message', $message);
     }
 
     /**
@@ -79,6 +311,26 @@ class ArticleController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $method = 'DELETE';
+
+        $url = config('qiita.url') . '/api/v2/items/' . $id;
+
+        $options = [
+            'headers' => [
+                'Authorization' => 'Bearer ' . config('qiita.token')
+            ]
+        ];
+
+        $client = new Client();
+
+        try {
+            $client->request($method, $url, $options);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            return back()->withErrors([
+                'error' => '削除処理を失敗しました'
+            ]);
+        }
+        return redirect()->route('articles.index')
+            ->with('flash_message', '記事を削除しました');
     }
 }
---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    @if (!empty($errors))
+        <div class="error">
+            @foreach($errors->all() as $error)
+                {{ $error }}
+            @endforeach
+        </div>
+    @endif
+
+    <form action="{{ route('articles.store') }}" method="post">
+        @csrf
+        <select name="private" id="private">
+            <option value="true" selected>非公開</option>
+            <option value="false" disabled>公開</option>
+        </select>
+        <div>
+            <input type="text" name="title" id="title" placeholder="タイトル" >
+        </div>
+        <div>
+            <input type="text" name="tags" id="tags" placeholder="知識に関する関するタグをスペース区切りで5つまで入力 (例:Ruby Rails)" required>
+        </div>
+        <div>
+            <textarea name="body" cols="30" rows="10" id="body" placeholder="エンジニアに関わる知識をMarkdown記法で書いて共有しよう" required></textarea>
+        </div>
+        <input type="submit" name="" id="" value="投稿する">
+    </form>
+    <button type="button" onclick="location.href='{{ route('articles.index') }}">一覧に戻る</button>
+</body>
+
+</html>
